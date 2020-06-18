dfb17e8185d0e8aed386d1533f724bca71b4d48c
TugnizzaMarco <marcot7@hotmail.it>
2020-06-18 10:58:42 +0200

    Starting commit

---
+++ Chat/src/servlets/ShoutServlet.java
@@ -0,0 +1,70 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ShoutServlet
+ */
+@WebServlet(urlPatterns = {"/shoutServlet"}, asyncSupported=true)
+public class ShoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private List<AsyncContext> contexts = new LinkedList<>();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShoutServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		final AsyncContext asyncContext = request.startAsync(request, response);
+        asyncContext.setTimeout(10 * 60 * 1000);
+        contexts.add(asyncContext);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<AsyncContext> asyncContexts = new ArrayList<>(this.contexts);
+        this.contexts.clear();
+		String name = request.getParameter("name");
+        String message = request.getParameter("message");
+        String htmlMessage = "<p><b>" + name + "</b><br/>" + message + "</p>";
+        ServletContext sc = request.getServletContext();
+        if (sc.getAttribute("messages") == null) {
+            sc.setAttribute("messages", htmlMessage);
+        } else {
+            String currentMessages = (String) sc.getAttribute("messages");
+            sc.setAttribute("messages", htmlMessage + currentMessages);
+        }
+        for (AsyncContext asyncContext : asyncContexts) {
+            try (PrintWriter writer = asyncContext.getResponse().getWriter()) {
+                writer.println(htmlMessage);
+                writer.flush();
+                asyncContext.complete();
+            } catch (Exception ex) {
+            }
+        }
+//        response.sendRedirect("index.jsp");
+	}
+
+}
